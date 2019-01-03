a7e5f0133d5bbcd06664d33dce65ea696b3810c5
Matt Doering <ablueplatypus21@gmail.com>
2019-01-03 10:21:10 -0500

    lots and lots of style

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,11 @@ class CartsController < ApplicationController
 
   def show
     @product_items = current_order.product_items
+    
+  end
+
+  def checkout
+    @product_items = current_order.product_items
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # patch '/cart', to: 'product_items#update', as: 'update_cart'
   resources :product_items, only: [:create, :update, :destroy]
   resource :cart, only: :show
+  get '/secure/checkout/HAHA', to: 'carts#checkout', as: 'checkout'
   # root to: "products#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
